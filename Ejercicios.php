a157b0267f1d1267501088d719df05d8b50f2919
alex4jh <160110005+alex4jh@users.noreply.github.com>
2024-05-09 17:30:36 +0200

    EJERCICIO DE EXTRAER NUMEROS PARES
    Resolución del ejercicio de acuerdo al enunciado

--- Ejercicios.php
+++ Ejercicios.php
@@ -1,1 +1,18 @@
 <?php
+
+//?  INGRESA UN NUMERO ENTERO EN EL CUAL SEA ALA CANTIDAD QUE INGRESE EXTRIGAS SOLOS LOS NUMEROS QUE SEAN PARES 
+
+
+function obtenerNumerosPares( int $numero) {
+
+    $digitos = str_split((string)$numero);
+    $numerosPares = [];
+    foreach ($digitos as $digito) {
+        if ($digito % 2 === 0) {
+            $numerosPares[] = $digito;
+        }
+    }
+    return implode('', $numerosPares);
+}
+
+echo obtenerNumerosPares(1155878511);
